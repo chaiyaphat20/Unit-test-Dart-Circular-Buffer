6be3395cd18a211248db9e773be9a3eb19f5539a
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-06-28 15:16:10 +0700

    test

--- lib/circular_buffer.dart
+++ lib/circular_buffer.dart
@@ -4,36 +4,8 @@ class CircularBuffer {
   int _indexRead = 0;
   int _indexWrite = 0;
   late List<int> _list;
-  CircularBuffer({int capacity = 3}) {
-    _capacity = capacity;
-    _list = List.generate(_capacity, (index) => 0);
-  }
 
   int count() {
     return _count;
-  }
-
-  int capacity() {
-    return _capacity;
-  }
-
-  void push(int value) {
-    _list[_indexWrite] = value;
-    _indexWrite++;
-    _indexWrite %= _capacity;
-    _count++;
-    if (overflow) {
-      read();
-    }
-  }
-
-  bool get overflow => _count > _capacity;
-
-  int read() {
-    int value = _list[_indexRead];
-    _indexRead++;
-    _indexRead %= _capacity;
-    _count--;
-    return value;
   }
 }
